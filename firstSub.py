adc3e392ab23916febc50c9bb897c4d845541d3a
Aaron Webb <aaron.f.webb@gmail.com>
2020-09-22 11:52:27 -0500

    initialize flower reco project

---
+++ firstSub.py
@@ -0,0 +1,19 @@
+from petal_helper import *
+
+import tensorflow as tf
+
+# Detect TPU, return appropriate distribution strategy
+try:
+    tpu = tf.distribute.cluster_resolver.TPUClusterResolver() 
+    print('Running on TPU ', tpu.master())
+except ValueError:
+    tpu = None
+
+if tpu:
+    tf.config.experimental_connect_to_cluster(tpu)
+    tf.tpu.experimental.initialize_tpu_system(tpu)
+    strategy = tf.distribute.experimental.TPUStrategy(tpu)
+else:
+    strategy = tf.distribute.get_strategy() 
+
+print("REPLICAS: ", strategy.num_replicas_in_sync)
